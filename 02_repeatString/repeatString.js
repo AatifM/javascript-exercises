48490ab91649e6842fb1ddd42a6b422d620e80c7
AatifM <119080567+AatifM@users.noreply.github.com>
2025-04-17 17:40:36 -0400

    Complete Assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num >= 0) {
+        let repeatedString = '';
 
+        for (let i = 0; i < num; i++) {
+            repeatedString += str;
+        }
+
+        return repeatedString;
+    }
+    
+    else {return 'ERROR';}
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+        return "ERROR";
+    }
 
+    if (max < min) {
+        [min, max] = [max, min];
+    }
+
+    let finalSum = 0;
+
+    for (let i = min; i <= max; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
